0d78bc1741785632e0804b8449ed85e72dd02acd
yilei <yilei@yileideMacBook-Pro.local>
2026-09-04 11:51:40 +0800

    release: Caiguang v0.3.37

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("sharpEyeDesktop", {
   platform: "macOS",
-  version: "0.3.35",
+  version: "0.3.37",
   fitWindow: (request) => ipcRenderer.invoke("caiguang:fit-window", request),
   getRuntimeStatus: () => ipcRenderer.invoke("caiguang:runtime-status"),
   checkForUpdate: () => ipcRenderer.invoke("caiguang:check-update"),
